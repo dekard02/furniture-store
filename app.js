0b72b6c369f95fe058d7a690ff6ce8703b1f239c
Dekard <dangkhoatanhao@gmail.com>
2023-02-04 23:52:50 +0700

    connect mongodb

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// const logger = require('')
 
+dotenv.config({ path: './.env' });
 const app = express();
 app.use('/public', express.static('./public'));
 
-const port = 8000;
-app.listen(port);
+mongoose.set('strictQuery', true);
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then(() => {
+  console.log('Connect DB successfully!!');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log('Server is running on ', PORT);
+});
